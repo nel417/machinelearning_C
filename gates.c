0a3d9914e869589a30c3b88eb218d6a15cd679b3
nickL <n1ck@hey.com>
2025-03-08 22:10:33 -0600

    refactor logic gates

--- gates.c
+++ gates.c
@@ -3,55 +3,64 @@
 #include <time.h>
 #include <math.h>
 
+#define EPS 1e-1
+#define LEARNING_RATE 1e-1
+#define ITERATIONS 100000
+
+typedef struct {
+    float weight1;
+    float weight2;
+    float bias;
+} NeuralNetwork;
+
 float sigmoidf(float x) {
     return 1.0f / (1.0f + expf(-x));
 }
 
 float train[][3] = {
-    {0,0,0},
-    {1,0,1},
-    {0,1,1},
-    {1,1,1},
-
+    {0, 0, 0},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 1},
 };
-#define train_count (sizeof(train)/sizeof(train[0]))
+
+#define train_count (sizeof(train) / sizeof(train[0]))
 
 float rand_float(void) {
-    return (float) rand() / (float) RAND_MAX;
+    return (float)rand() / (float)RAND_MAX;
 }
 
-float cost(float weight1, float weight2, float bias) {
+float cost(NeuralNetwork nn) {
     float result = 0.0f;
-    for(size_t i = 0; i < train_count; ++i) {
+    for (size_t i = 0; i < train_count; ++i) {
         float x1 = train[i][0];
         float x2 = train[i][1];
-        float y = sigmoidf(x1 * weight1 + x2 * weight2 + bias);
-        float d = y - train[i][2];    
-        result += d*d;
+        float y = sigmoidf(x1 * nn.weight1 + x2 * nn.weight2 + nn.bias);
+        float d = y - train[i][2];
+        result += d * d;
     }
-    result /= train_count;
-    return result;
+    return result / train_count;
+}
+
+void train_network(NeuralNetwork *nn) {
+    for (size_t i = 0; i < ITERATIONS; ++i) {
+        float c = cost(*nn);
+        float dw1 = (cost((NeuralNetwork){nn->weight1 + EPS, nn->weight2, nn->bias}) - c) / EPS;
+        float dw2 = (cost((NeuralNetwork){nn->weight1, nn->weight2 + EPS, nn->bias}) - c) / EPS;
+        float db = (cost((NeuralNetwork){nn->weight1, nn->weight2, nn->bias + EPS}) - c) / EPS;
+
+        nn->weight1 -= LEARNING_RATE * dw1;
+        nn->weight2 -= LEARNING_RATE * dw2;
+        nn->bias -= LEARNING_RATE * db;
+    }
 }
 
 int main() {
     srand(time(0));
-    float EPS = 1e-1;
-    float LEARNING_RATE = 1e-1;
-    int ITERATIONS = 100*1000;
-    float weight1 = rand_float();
-    float weight2 = rand_float();
-    float bias = rand_float();
+    NeuralNetwork nn = {rand_float(), rand_float(), rand_float()};
 
-    for(size_t i = 0; i < ITERATIONS; ++i) {
-        float c = cost(weight1, weight2, bias);
-        float dw1 = (cost(weight1 + EPS, weight2, bias) - c) / EPS;
-        float dw2 = (cost(weight1, weight2 + EPS, bias) - c) / EPS;
-        float db = (cost(weight1, weight2, bias + EPS) - c) / EPS;
-        weight1 -= LEARNING_RATE*dw1;
-        weight2 -= LEARNING_RATE*dw2;
-        bias -= LEARNING_RATE*db;
-    }
-    printf("cost = %f, weight1 = %f, weight2 = %f, bias = %f\n", cost(weight1, weight2, bias), weight1, weight2, bias);
+    train_network(&nn);
+    printf("cost = %f, weight1 = %f, weight2 = %f, bias = %f\n", cost(nn), nn.weight1, nn.weight2, nn.bias);
 
     return 0;
 }
